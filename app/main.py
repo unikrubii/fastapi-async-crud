1e21c63f6c9c0a981f7c606ef9ff9eb48d496bf9
Sirilert Thitikul <rubii@Rubii-Mac.local>
2024-11-09 23:03:15 +0700

    Update main.py and remove unused files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 
-app = FastAPI()
+from app.router import item
+from app.db import lifespan
 
+app = FastAPI(lifespan=lifespan)
+app.include_router(item.router)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    '''
+    Function to handle exception from fastapi when get invalid input
+    '''
+    # Create a custom response that returns a 400 Bad Request
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={'detail': 'Invalid input data', 'errors': exc.errors()},
+    )
 
 @app.get('/')
 async def root():

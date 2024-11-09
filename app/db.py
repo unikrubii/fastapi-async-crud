493384a1a171d46eb7118a7fcd7a48eba5844f39
Sirilert Thitikul <rubii@Rubii-Mac.local>
2024-11-10 01:29:38 +0700

    Fix docstring of lifespan function

--- app/db.py
+++ app/db.py
@@ -4,10 +4,10 @@ from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    """
+    '''
     A lifespan context manager to initialize and close the database connection.
     This will be used to ensure the database is ready when the app starts and closed when the app shuts down.
-    """
+    '''
     # Initialize the database connection
     await Tortoise.init(
         db_url="sqlite://db.sqlite3",
